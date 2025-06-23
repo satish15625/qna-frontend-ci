17a332f61b9633be6391477af8abd206d808d63b
unknown <satish15625@gmail.com>
2025-06-23 16:35:05 +0530

    added the new React js api file

--- src/api.js
+++ src/api.js
@@ -1,13 +1,35 @@
+// export const askQuestion = async (question) => {
+//   try {
+//     const response = await fetch("http://192.168.1.3:8000/query/", {
+      
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ question }),
+//     });
+//     const data = await response.json();
+//     return data.answer;
+//   } catch (error) {
+//     console.error("Error:", error);
+//     return "Error: Unable to get response from server.";
+//   }
+// };
+
+
 export const askQuestion = async (question) => {
   try {
+    const body = JSON.stringify({ query: question });  // ✅ FIXED KEY
+    console.log("Sending:", body);
+
     const response = await fetch("http://192.168.1.3:8000/query/", {
-      
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ question }),
+      body: body,
     });
+
     const data = await response.json();
     return data.answer;
   } catch (error) {
